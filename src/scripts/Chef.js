6549e52a8980ce08273bd729cffc9895d0849dbf
FelixLapointe13 <lapointefelix13@gmail.com>
2026-02-26 14:17:25 -0500

    Class chef constructor fait

--- src/scripts/Chef.js
+++ src/scripts/Chef.js
@@ -1,5 +1,9 @@
 export default class Chef {
-  constructor() {
+  constructor(element) {
+    this.element = element;
+    this.menu = [];
+    this.container = document.querySelector('.chef__order');
+
     this.init();
   }
 
--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -8,7 +8,7 @@ class Main {
 
   init() {
     Icons.load();
-    const chefs = document.querySelectorAll('chef');
+    const chefs = document.querySelectorAll('.chef');
     for (let i = 0; i < chefs.length; i++) {
       const chef = chefs[i];
       new Chef(chef);
